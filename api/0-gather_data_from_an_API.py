b67bb55b19a2d2cbae778c40aedb78b181222e5e
maxnchief <logandogan@gmail.com>
2024-11-08 13:29:39 -0600

    think i got it

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -45,7 +45,7 @@ def get_employee_todo_progress(employee_id):
 
     # Display the TODO list progress
     print(f"Employee {employee_name} 
-          is done with tasks({number_of_done_tasks}/{total_tasks}):")
+    is done with tasks({number_of_done_tasks}/{total_tasks}):")
     for task in done_tasks:
         print(f"\t {task.get('title')}")
 
