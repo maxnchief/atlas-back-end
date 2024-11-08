734844794d918320c5780d347e099ffac4d6e4d1
maxnchief <logandogan@gmail.com>
2024-11-08 13:30:14 -0600

    "womp"

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -44,8 +44,7 @@ def get_employee_todo_progress(employee_id):
     number_of_done_tasks = len(done_tasks)
 
     # Display the TODO list progress
-    print(f"Employee {employee_name} 
-    is done with tasks({number_of_done_tasks}/{total_tasks}):")
+    print(f"Employee {employee_name} is done with tasks({number_of_done_tasks}/{total_tasks}):")
     for task in done_tasks:
         print(f"\t {task.get('title')}")
 
